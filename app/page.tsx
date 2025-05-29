9530ddd00c1ab14a86794b7dfa125fb514716c7d
nicholaskim7 <nickdraggy@gmail.com>
2025-05-28 20:21:42 -0500

    new date object so user navigates to the current month automatically

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
+import Link from "next/link";
 import styles from "./css/landing.module.css";
 import CheckBoxIcon from '@mui/icons-material/CheckBox';
 
 export default function Home() {
+  const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+  const m = new Date(); // get the current date
+  let name = month[m.getMonth()]; // get the name of the current month
 
   return (
     <div className={styles.container}>
       <h1 className={styles.text}><span className={styles.color1}>Check</span><span className={styles.color2}>mate</span> <CheckBoxIcon className={styles.icon}/></h1>
-      <a href="/productivity/january" className={styles.link}>Get started</a>
+      <Link href={`/productivity/${name}`} className={styles.link}>Get started</Link>
     </div>
   );
 }
